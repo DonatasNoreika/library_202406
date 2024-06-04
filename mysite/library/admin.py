27c020016fb4aa7f5584a4b9d2ac3c4aeef403e1
DonatasNoreika <donoras@gmail.com>
2024-06-04 10:24:32 +0300

    +Sukurta knygų egzempliorių inline +Sukurta Autoriaus ModelAdmin

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Genre, Author, Book, BookInstance
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'display_books']
+
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+    can_delete = False
+    readonly_fields = ['uuid']
+    fields = ['uuid', 'due_back', 'status']
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'isbn', 'display_genre']
-
+    inlines = [BookInstanceInLine]
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ['uuid', 'book', 'status', 'due_back']
@@ -16,6 +27,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Genre)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance, BookInstanceAdmin)
