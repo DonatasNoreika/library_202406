2374677f176d79984fe92340bda455e7f1cb83c6
DonatasNoreika <donoras@gmail.com>
2024-06-06 09:53:05 +0300

    4 pridėtas knygų sąrašas

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, BookInstance, Author
 import datetime
+from django.views import generic
 
 
 # Create your views here.
@@ -34,3 +35,9 @@ def author(request, author_id):
         'author': author,
     }
     return render(request, template_name="author.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = "books.html"
+    context_object_name = "books"
